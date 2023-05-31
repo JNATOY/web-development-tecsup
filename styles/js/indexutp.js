9be2fb721a581ea3c788b744659d4ade4449e424
JNATOY <jcnatoyepez@gmail.com>
2023-05-31 01:42:09 -0500

    java

---
+++ styles/js/indexutp.js
@@ -0,0 +1,81 @@
+for (let contador = 0; contador < 12; contador++) {
+    console.log(12 * (contador + 1));
+  }
+
+
+/* -------------------objetos--------------------- */
+/* const person = {
+    firstName: 'Elliot',
+    lastName: 'Garamendi',
+    age: 27,
+    single: true,
+    special: () => { }
+  };
+  
+  console.log(person);
+  console.log(person.firstName);
+  console.log('Nombre: ' + person.firstName + ', Apellido: ' + person.lastName);
+  console.log(`
+    Nombre: ${person.firstName},
+    Apellido: ${person.lastName}
+    Edad: ${person.age},
+    Soltero: Sí,
+    `);
+
+
+ */
+
+
+
+/* var firstName;
+let lastName;
+const age = 26;
+const MONTHS_OF_THE_YEAR = 12;
+
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+
+firstName = 'Juan Ñato';
+console.log(firstName);
+
+firstName = 17;
+// age = 27;
+console.log(firstName);
+console.log(age);
+console.log(MONTHS_OF_THE_YEAR);
+ */
+
+
+
+
+
+/* 
+ejemplo 1
+
+console.log (1 + 'juan ñato');
+console.log (8 +8);
+console.log ('8'*8);
+console.log ('8'>8);
+console.log (false && true);
+console.log (false 	||  true);
+
+
+ejemplo 2
+
+
+/* console.log(1 + ' Elliot Garamendi');
+console.log(8 + 8);
+console.log('8' * 8);
+console.log('8' > 8);
+console.log(false && true);
+console.log(false || true);
+console.log(typeof 8);
+console.log(typeof 'Elliot');
+console.log(typeof true);
+console.log(typeof undefined);
+console.log(typeof null);
+console.log(typeof {});
+console.log(typeof []);
+console.log(typeof function () { });
+alert('Hola mundo'); */
