03bf8d716e39d4aa562de7a435984bdd07e80901
JNATOY <jcnatoyepez@gmail.com>
2023-06-07 23:02:34 -0500

    js terminado

--- semana6/moonlight-develop (2)/moonlight-develop/js/index.js
+++ semana6/moonlight-develop (2)/moonlight-develop/js/index.js
@@ -15,12 +15,6 @@ window.addEventListener('DOMContentLoaded', () => {
   const heroMoon = document.querySelector('.hero__moon-image-container');
   const heroMountainsBehind = document.querySelector('.hero__mountains-behind-image-container');
   const heroMountainsFront = document.querySelector('.hero__mountains-front-image-container');
-  console.log(heroTitle);
-  console.log(heroLearnButton);
-  console.log(heroStarts);
-  console.log(heroMoon);
-  console.log(heroMountainsBehind);
-  console.log(heroMountainsFront);
 
   const openMenu = () => linksMenu.classList.add('header-nav__menu-link-list--open');
 
@@ -33,10 +27,13 @@ window.addEventListener('DOMContentLoaded', () => {
   });
 
   window.addEventListener('scroll', () => {
-    header.classList.toggle('header--scroll', window.scrollY > 0);
-    headerNav.classList.toggle('header-nav--scroll', window.scrollY > 0);
+    header.classList.toggle('header--scroll', scrollY > 0);
+    headerNav.classList.toggle('header-nav--scroll', scrollY > 0);
 
-
+    heroTitle.style.transform = `translate(-${scrollY}px, ${0})`
+    // heroTitle.style.top = `${scrollY * 0.375}px`;
+    // heroTitle.style.left = `${50 - (scrollY * 0.375)}%`;
+    // heroLearnButton.style.top = `${50 + (scrollY * 0.125)}%`;
   });
 
   menuButton.addEventListener('click', openMenu);
