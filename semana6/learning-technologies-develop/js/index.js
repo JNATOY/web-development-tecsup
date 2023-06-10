106e81ae4879b2bf79bfb367d57de89419bcd451
JNATOY <jcnatoyepez@gmail.com>
2023-06-09 21:39:53 -0500

    slider

--- semana6/learning-technologies-develop/js/index.js
+++ semana6/learning-technologies-develop/js/index.js
@@ -1,7 +1,9 @@
 'use strict';
 
 import header from "./modules/header.js";
+import slider from "./modules/slider.js";
 
 window.addEventListener('DOMContentLoaded', () => {
     header();
+    slider();
 });
