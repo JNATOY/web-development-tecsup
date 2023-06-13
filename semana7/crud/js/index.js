f22af1786a908d3af3526ce0a015bb289f987fb6
JNATOY <jcnatoyepez@gmail.com>
2023-06-12 21:30:51 -0500

    grud

--- semana7/crud/js/index.js
+++ semana7/crud/js/index.js
@@ -1,1 +1,24 @@
-alert ('hola mundo');
+'use strict';
+
+window.addEventListener('DOMContentLoaded', () => {    /* DESPUES QUE CARGA EL DOM EJECUTA  EL CONSOLE */
+    console.log(document.forms['pokemonForm']);         /* BUSCA EN TODO EL DOM FORMULARIOS QUE TENGAN EL NAME POKEMON FORM */
+    pokemonForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const name = pokemonForm['name'].value;       /* esto es el objeto :  pokemonForm['name'] ; CUANDO SELECCIONO EL FORMULARIO QUIERO OBTENER UN VALOR */
+        const type = pokemonForm['type'].value;
+        const image = pokemonForm['image'].value;
+
+        pokemonTBody.innerHTML += `
+        <tr>
+        <td>n</td>
+        <td>${name}</td>
+        <td><span class="badge text-bg-danger">${type}</span></td>
+        <td>
+            <img src="${image}" alt="Charmander" width="32"
+            height="32" />
+        </td>
+        </tr>
+    `;                                    /* el += es para que el contenido que se agrega se ponga al final */
+        pokemonForm.reset();       /* borra todo lo que se ingreso en el formulario */
+    });
+});
