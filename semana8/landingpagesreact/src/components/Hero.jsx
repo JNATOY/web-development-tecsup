0a34df5305e7171e0befc76794dca75a664106c9
JNATOY <jcnatoyepez@gmail.com>
2023-06-23 10:43:36 -0500

    finalll

--- semana8/landingpagesreact/src/components/Hero.jsx
+++ semana8/landingpagesreact/src/components/Hero.jsx
@@ -5,8 +5,8 @@ const Hero = () => {
             <div className="container">
                 <div className="card" style={{ position: 'relative' }}>
                     <div className="card__body">
-                        <h1 className="card__subtitle">Elliot Garamendi</h1>
-                        <h2 className="card__title">El mejor Frontend Developer y Docente</h2>
+                        <h1 className="card__subtitle">Juan Ñato</h1>
+                        <h2 className="card__title">El mejor Frontend Developer del Perú</h2>
                         <p className="card__text">Desarrollador Web Frontend con React y Next.js. Docente de especialización en Desarrollo Web Frontend. Apasionado del autoaprendizaje y del código limpio.</p>
                         <div className="d-flex gap-2xs">
                             <button className="button button--outline-primary">Descargar CV</button>
