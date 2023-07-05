4526d2859196ba87dd5472bdf7b8b4c086b19b1c
JNATOY <jcnatoyepez@gmail.com>
2023-07-04 23:14:34 -0500

    finaaa

---
+++ semana10/tiendita-feliz/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <section className='py-12'>
+      <div className="container p-4 mx-auto">
+        <h1 className='text-sky-700 text-3xl text-center'>🛒 Tiendita Feliz 🛒</h1>
+      </div>
+    </section>
+  )
+}
+
+export default App
