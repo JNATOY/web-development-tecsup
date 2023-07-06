42939803b38b4770a572b940883f301ba2112c22
JNATOY <jcnatoyepez@gmail.com>
2023-07-05 20:59:56 -0500

    finall

--- semana10/tiendita-feliz/src/App.jsx
+++ semana10/tiendita-feliz/src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css'
+import Products from './components/Products';
+import { products } from './mocks/products.json';
 
 function App() {
   return (
     <section className='py-12'>
-      <div className="container p-4 mx-auto">
-        <h1 className='text-sky-700 text-3xl text-center'>🛒 Tiendita Feliz 🛒</h1>
+      <div className="container p-4 mx-auto flex flex-col gap-8">
+        <h1 className='text-sky-600 text-3xl font-black text-center'>🛒 Tiendita Feliz 🛒</h1>
+        <Products data={products} />
       </div>
     </section>
   )
---
+++ semana10/tiendita-feliz/src/components/Products.jsx
@@ -0,0 +1,36 @@
+import { formatCurrency } from "../utils/utils";
+
+const Products = ({ data }) => {
+    console.log(data);
+    return (
+        <ul className="grid grid-cols-[repeat(auto-fit,_minmax(256px,_1fr))] gap-6">
+            {data.map(element => {
+                const { id, title, price, discountPercentage, rating, brand, thumbnail } = element;
+                return (
+                    <li
+                        key={id}
+                        className="rounded-2xl overflow-hidden transition-shadow hover:shadow-xl hover:shadow-sky-600"
+                    >
+                        <img
+                            src={thumbnail}
+                            alt={title}
+                            width={256}
+                            height={256}
+                            className="w-full h-48 object-cover"
+                        />
+                        <div className="h-64 p-3 bg-zinc-800 flex flex-col gap-0.5">
+                            <h2 className="text-zinc-400 font-bold">{brand}</h2>
+                            <h3 className="text-sky-600 text-xl font-extrabold">{title}</h3>
+                            <span className="text-zinc-400 text-sm font-semibold">{rating}</span>
+                            <h4><span className="line-through">{formatCurrency(price)}</span> <sup className="py-0.5 px-2 rounded-lg bg-sky-900">{discountPercentage * -1}%</sup></h4>
+                            <h5 className="text-sky-600 text-lg font-bold">{formatCurrency(price * (100 - discountPercentage) / 100)}</h5>
+                            <button className="py-2 px-8 mt-auto rounded-2xl bg-sky-600 font-semibold transition-colors hover:bg-sky-500">Comprar</button>
+                        </div>
+                    </li>
+                );
+            })}
+        </ul>
+    );
+};
+
+export default Products;
